65d2b98a01a887ad7ff5be48ead8d437e84d6aea
anra54 <anugrah.ramadhani@gmail.com>
2020-02-26 16:47:17 +0700

    Validate Inn Tax: autofill_inn_tax_value

--- inn/hooks.py
+++ inn/hooks.py
@@ -79,13 +79,16 @@ app_license = "MIT"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-#	}
-# }
+doc_events = {
+	# 	"*": {
+	# 		"on_update": "method",
+	# 		"on_cancel": "method",
+	# 		"on_trash": "method"
+	#	}
+	"Inn Tax": {
+		"validate": "inn.inn.doctype.inn_tax.inn_tax.autofill_inn_tax_value"
+	},
+}
 
 # Scheduled Tasks
 # ---------------
