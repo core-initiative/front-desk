941be2de4793a9e2589b55c70d8308087e14dfc0
anra54 <anugrah.ramadhani@gmail.com>
2020-03-05 16:56:18 +0700

    Start Check In progress

--- inn/inn_hotels/doctype/inn_reservation/inn_reservation.js
+++ inn/inn_hotels/doctype/inn_reservation/inn_reservation.js
@@ -1,8 +1,58 @@
 // Copyright (c) 2020, Core Initiative and contributors
 // For license information, please see license.txt
+var is_check_in = getUrlVars()['is_check_in'];
 
 frappe.ui.form.on('Inn Reservation', {
 	refresh: function(frm) {
+		// Hide some variables that not needed to be filled first time Reservation Created
+		if (frm.doc.__islocal == 1) {
+			console.log("notsaved");
+			frm.set_df_property('arrival', 'hidden', 1);
+			frm.set_df_property('departure', 'hidden', 1);
+			frm.set_df_property('actual_room_rate', 'hidden', 1);
+			frm.set_df_property('sb1', 'hidden', 1); // Actual Room Rate Breakdown Section
+		}
+		// Reservation is Saved, and status Reserved
+		if (frm.doc.__islocal != 1 && frm.doc.status == 'Reserved') {
+			console.log("is saved");
+			// Set all variables that hidden to be shown again
+			frm.set_df_property('arrival', 'hidden', 0);
+			frm.set_df_property('departure', 'hidden', 0);
+			frm.set_df_property('actual_room_rate', 'hidden', 0);
 
-	}
+			// Show Start Check In Process button if is_check_in flag undefined
+			if (is_check_in == undefined) {
+				console.log("is_check_in undefined");
+				frm.add_custom_button(__("Start Check In Process"), function () {
+					frappe.call({
+						method:"inn.inn_hotels.doctype.inn_reservation.inn_reservation.start_check_in",
+						args: {
+							source: 'check_in_button',
+							reservation: frm.doc.name
+						},
+						callback: (r) => {
+							if (r.message) {
+								window.location.href = r.message;
+							}
+						}
+					});
+				});
+			}
+			// Show Info that Check In is In Progress. Meaning button Check In clicked, either from Reservation List or
+			// from Start Check In Process Button
+			if (is_check_in == "true") {
+				console.log("masuk sini woy");
+				frm.set_intro(__("In Progress Checking In Guest"));
+			}
+		}
+	},
 });
+
+// Function to extract variable's value passed on URL
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
--- inn/inn_hotels/doctype/inn_reservation/inn_reservation.py
+++ inn/inn_hotels/doctype/inn_reservation/inn_reservation.py
@@ -4,7 +4,30 @@
 
 from __future__ import unicode_literals
 import frappe
+import json
 from frappe.model.document import Document
 
 class InnReservation(Document):
 	pass
+
+@frappe.whitelist()
+def check_in_reservation(reservation_id):
+	doc = frappe.get_doc('Inn Reservation', reservation_id)
+	if (doc.status == 'Reserved'):
+		doc.status = 'In House'
+		doc.save()
+
+	return doc.status
+
+@frappe.whitelist()
+def start_check_in(source, reservation):
+	if source == 'list':
+		reservation_id = json.loads(reservation)[0]
+	elif source == 'check_in_button':
+		reservation_id = reservation
+	if frappe.db.get_value('Inn Reservation', reservation_id, 'status') == 'Reserved':
+		doc = frappe.get_doc('Inn Reservation', reservation_id)
+		# TODO: Create Folio based on this reservation
+		return frappe.utils.get_url_to_form('Inn Reservation', reservation_id) + '?is_check_in=true'
+	else:
+		frappe.msgprint("Reservation Status must be Reserved in order to be Checked In")
