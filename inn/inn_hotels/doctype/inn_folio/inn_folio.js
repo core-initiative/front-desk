29222b91a14d98922a9202152af3f351800ce21c
anra54 <anugrah.ramadhani@gmail.com>
2020-03-09 16:26:49 +0700

    Folio button: add package, add charge, add payment

--- inn/inn_hotels/doctype/inn_folio/inn_folio.js
+++ inn/inn_hotels/doctype/inn_folio/inn_folio.js
@@ -2,7 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Inn Folio', {
-	refresh: function(frm) {
+	onload: function(frm) {
+		frm.get_field("folio_transaction").grid.only_sortable();
+	},
+	add_charge: function (frm) {
+		var w = window.open(frappe.urllib.get_full_url('/desk#Form/Inn%20Folio%20Transaction/New%20Inn%20Folio%20Transaction%201?flag=Debit&parent=' + frm.doc.name));
 
+		if (!w) {
+			frappe.msgprint(__("Please enable pop-ups")); return;
+		}
+	},
+	add_payment: function (frm) {
+		var w = window.open(frappe.urllib.get_full_url('/desk#Form/Inn%20Folio%20Transaction/New%20Inn%20Folio%20Transaction%201?flag=Credit&parent=' + frm.doc.name));
+
+		if (!w) {
+			frappe.msgprint(__("Please enable pop-ups")); return;
+		}
 	}
 });
--- inn/inn_hotels/doctype/inn_folio_transaction/inn_folio_transaction.js
+++ inn/inn_hotels/doctype/inn_folio_transaction/inn_folio_transaction.js
@@ -1,8 +1,23 @@
 // Copyright (c) 2020, Core Initiative and contributors
 // For license information, please see license.txt
+var parent = getUrlVars()['parent'];
 
 frappe.ui.form.on('Inn Folio Transaction', {
-	refresh: function(frm) {
-
-	}
+	before_save: function(frm) {
+		if (parent) {
+			frm.doc.parent = parent;
+			frm.doc.parenttype = 'Inn Folio';
+			frm.doc.parentfield = 'folio_transaction';
+		}
+	},
 });
+
+
+// Function to extract variable's value passed on URL
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
