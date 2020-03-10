feed1181b982b9151dc8e3c049cc4ebd02d217d7
anra54 <anugrah.ramadhani@gmail.com>
2020-03-10 16:09:46 +0700

    Doctype Inn room booking

---
+++ inn/inn_hotels/doctype/inn_room_booking/test_inn_room_booking.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+// rename this file from _test_[name] to test_[name] to activate
+// and remove above this line
+
+QUnit.test("test: Inn Room Booking", function (assert) {
+	let done = assert.async();
+
+	// number of asserts
+	assert.expect(1);
+
+	frappe.run_serially([
+		// insert a new Inn Room Booking
+		() => frappe.tests.make('Inn Room Booking', [
+			// values to be set
+			{key: 'value'}
+		]),
+		() => {
+			assert.equal(cur_frm.doc.key, 'value');
+		},
+		() => done()
+	]);
+
+});
