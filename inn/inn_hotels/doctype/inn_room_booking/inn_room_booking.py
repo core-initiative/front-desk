b1d2ae2b570d2c90ae6e30fbb4217bed82defeaf
anra54 <anugrah.ramadhani@gmail.com>
2020-03-11 17:15:44 +0700

    filter room available in room booking

--- inn/inn_hotels/doctype/inn_room_booking/inn_room_booking.py
+++ inn/inn_hotels/doctype/inn_room_booking/inn_room_booking.py
@@ -6,25 +6,31 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+
 class InnRoomBooking(Document):
 	pass
 
 
 @frappe.whitelist()
+# Keep the Room Booking valid when Reservation created or updated
 def update_by_reservation(reservation_id):
 	return_message = ''
 	reservation_doc = frappe.get_doc('Inn Reservation', reservation_id)
 	if reservation_doc.status == 'Cancel':
+		# If Reservation Canceled, set the Room Booking Status to Canceled
 		room_booking_doc = frappe.get_doc('Inn Room Booking',
 										  {'reference_type': 'Inn Reservation', 'reference_name': reservation_id})
 		room_booking_doc.status = 'Canceled'
 		return_message = room_booking_doc.name + 'Status is Canceled by Cancelling Reservation: ' + reservation_id
 	elif reservation_doc.status == 'Finish':
+		# If Reservation Finished (Guest successfully checking out) set the Room Booking Status to Finished
 		room_booking_doc = frappe.get_doc('Inn Room Booking',
 										  {'reference_type': 'Inn Reservation', 'reference_name': reservation_id})
 		room_booking_doc.status = 'Finished'
 		return_message = room_booking_doc.name + ' Status is Finished by Checking Out Reservation: ' + reservation_id
 	elif reservation_doc.status == 'In House':
+		# Guest successfully Checking In. Set Room Booking Status to Stayed, Update start, end, and room_id according
+		# to the most valid info in Reservation
 		room_booking_doc = frappe.get_doc('Inn Room Booking',
 										  {'reference_type': 'Inn Reservation', 'reference_name': reservation_id})
 		if reservation_doc.arrival != room_booking_doc.start:
@@ -40,9 +46,11 @@ def update_by_reservation(reservation_id):
 		pass
 	# TODO: update room booking if reservation no show
 	else:
+		# Reservation status is Reserved
 		if not frappe.db.exists('Inn Room Booking',
 								{'reference_type': 'Inn Reservation', 'reference_name': reservation_id}):
-			# Create new Room Booking if Reservation is created first time
+			# Room Booking is not created yet. New Reservation with status Reserved, create new Room Booking with the info
+			# from Reservation details and Status is Booked
 			room_booking_doc = frappe.new_doc('Inn Room Booking')
 			room_booking_doc.start = reservation_doc.expected_arrival
 			room_booking_doc.end = reservation_doc.expected_departure
@@ -54,7 +62,7 @@ def update_by_reservation(reservation_id):
 			room_booking_doc.insert()
 			return_message = 'Created New Room Booking: ' + room_booking_doc.name
 		else:
-			# Update room booking if reservation got updated
+			# Update room booking if reservation got updated, while guest is still not Checking In
 			room_booking_doc = frappe.get_doc('Inn Room Booking',
 											  {'reference_type': 'Inn Reservation', 'reference_name': reservation_id})
 			reservation_changed = False
@@ -71,3 +79,117 @@ def update_by_reservation(reservation_id):
 				room_booking_doc.save()
 				return_message = room_booking_doc.name + ' Updated by changes in Reservation: ' + reservation_doc.name
 	return return_message
+
+
+@frappe.whitelist()
+def get_room_available(doctype, txt, searchfield, start, page_len, filters):
+	room_list = []
+	if filters.get('phase') == 'Check In':
+		if filters.get('start') and filters.get('end'):
+			if filters.get('room_type'):
+				if filters.get('bed_type'):
+					room_list = list(frappe.db.sql(
+						"select name, room_type, bed_type from `tabInn Room` where room_status = 'Vacant Ready' and room_type = %s and bed_type = %s",
+						(filters.get('room_type'), filters.get('bed_type'))
+					))
+				else:
+					room_list = list(frappe.db.sql(
+						"select name, room_type, bed_type from `tabInn Room` where room_status = 'Vacant Ready' and room_type = %s",
+						(filters.get('room_type'))
+					))
+			else:
+				room_list = list(frappe.db.sql(
+					"select name, room_type, bed_type from `tabInn Room` where room_status = 'Vacant Ready' "
+				))
+	else:
+		if filters.get('start') and filters.get('end'):
+			if filters.get('room_type'):
+				if filters.get('bed_type'):
+					room_list = list(frappe.db.sql(
+						"select name, room_type, bed_type from `tabInn Room` where room_type = %s and bed_type = %s",
+						(filters.get('room_type'), filters.get('bed_type'))
+					))
+				else:
+					room_list = list(frappe.db.sql(
+						"select name, room_type, bed_type from `tabInn Room` where room_type = %s",
+						(filters.get('room_type'))
+					))
+			else:
+				room_list = list(frappe.db.sql(
+					"select name, room_type, bed_type from `tabInn Room`"
+				))
+
+	room_book_list = get_room_book_list(filters)
+
+	for room_book in room_book_list:
+		for item in room_list:
+			if item[0] == room_book[0]:
+				room_list.remove(item)
+
+	return room_list
+
+@frappe.whitelist()
+def get_room_type_available(doctype, txt, searchfield, start, page_len, filters):
+	room_list = get_room_available(doctype, txt, searchfield, start, page_len, filters)
+
+	tmp = []
+	room_type_list = []
+
+	for room in room_list:
+		tmp.append(room[1])
+
+	tmp = list(set(tmp))
+
+	for t in tmp:
+		room_type_list.append([t])
+
+	return room_type_list
+
+
+@frappe.whitelist()
+def get_bed_type_available(doctype, txt, searchfield, start, page_len, filters):
+	room_list = get_room_available(doctype, txt, searchfield, start, page_len, filters)
+
+	tmp = []
+	bed_type_list = []
+
+	for room in room_list:
+		if room[1] == filters.get('room_type'):
+			tmp.append(room[2])
+
+	tmp = list(set(tmp))
+
+	for t in tmp:
+		bed_type_list.append([t])
+
+	return bed_type_list
+
+
+def get_room_book_list(filters):
+	room_book_list = list(frappe.db.sql(
+		"select irb.room_id from `tabInn Room Booking` as irb "
+		"where irb.reference_name != %s "
+		"and irb.status = 'Booked' "
+		"and irb.room_availability = 'Room Sold' "
+		"and irb.start != irb.end "
+		"and ((%s >= irb.start and %s < irb.end) "
+		"or (%s > irb.start and %s <= irb.end) "
+		"or (%s < irb.start and %s > irb.end))",
+		(filters.get('reference_type'),
+		 filters.get('start'), filters.get('start'),
+		 filters.get('end'), filters.get('end'),
+		 filters.get('start'), filters.get('end'))
+	))
+	room_book_list.extend(list(frappe.db.sql(
+		"select irb.room_id from `tabInn Room Booking` as irb "
+		"where irb.status = 'Booked' "
+		"and irb.room_availability in ('Under Construction', 'Office Use', 'Out of Order', 'House Use', 'Room Compliment') "
+		"and irb.start != irb.end "
+		"and ((%s >= irb.start and %s < irb.end) "
+		"or (%s > irb.start and %s <= irb.end) "
+		"or (%s < irb.start and %s > irb.end))",
+		(filters.get('start'), filters.get('start'),
+		 filters.get('end'), filters.get('end'),
+		 filters.get('start'), filters.get('end'))
+	)))
+	return room_book_list
