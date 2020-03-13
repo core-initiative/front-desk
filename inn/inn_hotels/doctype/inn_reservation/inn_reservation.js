8384edcf5c6eecc2c963ec671806d780571cca41
anra54 <anugrah.ramadhani@gmail.com>
2020-03-13 17:39:56 +0700

    fix: #17

--- inn/inn_hotels/doctype/inn_reservation/inn_reservation.js
+++ inn/inn_hotels/doctype/inn_reservation/inn_reservation.js
@@ -236,13 +236,20 @@ frappe.ui.form.on('Inn Reservation', {
 	},
 	actual_room_rate: function (frm) {
 		if (frm.doc.arrival != undefined && frm.doc.departure != undefined) {
-			if (parseFloat(frm.doc.actual_room_rate) > 0 && parseFloat(frm.doc.actual_room_rate) < parseFloat(frm.doc.base_room_rate)) {
+			if (parseFloat(frm.doc.actual_room_rate) > 0 && parseFloat(frm.doc.actual_room_rate) <= parseFloat(frm.doc.base_room_rate)) {
 				frappe.msgprint("Actual Room Rate must be equal or higher than Base Room Rate.");
 				frm.set_value('actual_room_rate', 0);
 				frm.set_value('room_bill',0);
-				frm.set_value('actual_room_rate_tax', 0);
+				frm.set_value('actual_room_rate_tax', null);
 				frm.set_value('nett_actual_room_rate', 0);
-				frm.set_value('actual_breakfast_rate_tax', 0);
+				frm.set_value('actual_breakfast_rate_tax', null);
+				frm.set_value('nett_actual_breakfast_rate', 0);
+			}
+			else if (parseFloat(frm.doc.actual_room_rate) == 0) {
+				frm.set_value('room_bill',0);
+				frm.set_value('actual_room_rate_tax', null);
+				frm.set_value('nett_actual_room_rate', 0);
+				frm.set_value('actual_breakfast_rate_tax', null);
 				frm.set_value('nett_actual_breakfast_rate', 0);
 			}
 			else {
