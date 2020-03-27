1a104607b2e151859bc3a9ec009bbebe71245d8d
anra54 <anugrah.ramadhani@gmail.com>
2020-03-27 13:13:22 +0700

    inn shift: populate shift content

--- inn/inn_hotels/doctype/inn_shift/inn_shift.js
+++ inn/inn_hotels/doctype/inn_shift/inn_shift.js
@@ -6,12 +6,16 @@ var total_payment = 0;
 var total_refund = 0;
 
 frappe.ui.form.on('Inn Shift', {
+	refresh: function(frm) {
+		frm.set_df_property('sb5', 'hidden', 1);
+	},
 	onload: function(frm) {
 		frm.set_df_property('total_cash_qty', 'hidden', 0);
 		frm.get_field('cc_detail').grid.cannot_add_rows = true;
 		frm.get_field('payment_detail').grid.cannot_add_rows = true;
 		frm.get_field('refund_detail').grid.cannot_add_rows = true;
 		if (frm.doc.__islocal === 1) {
+			frm.set_df_property('sb4', 'hidden', 1);
 			frappe.call({
 				method: 'inn.inn_hotels.doctype.inn_shift.inn_shift.is_there_open_shift',
 				callback: (r) => {
@@ -32,6 +36,16 @@ frappe.ui.form.on('Inn Shift', {
 					}
 				}
 			});
+			populate_payment_refund(frm, null);
+		}
+		else {
+			if (frm.doc.status === 'Open') {
+				populate_payment_refund(frm, frm.doc.name);
+			}
+			else {
+				set_all_read_only();
+				frm.disable_save();
+			}
 		}
 	}
 });
@@ -52,3 +66,86 @@ frappe.ui.form.on('Inn CC Detail',{
 		frm.set_value('total_cash_qty', total_cash_qty);
 	}
 });
+
+function set_all_read_only() {
+	cur_frm.set_df_property('close_shift', 'hidden', 1);
+	cur_frm.get_field("cc_detail").grid.only_sortable();
+	cur_frm.get_field("payment_detail").grid.only_sortable();
+	cur_frm.get_field("refund_detail").grid.only_sortable();
+	frappe.meta.get_docfield('Inn CC Detail', 'qty', cur_frm.docname).read_only = true;
+}
+
+function populate_payment_refund(frm, shift_id) {
+	frappe.call({
+		method: 'inn.inn_hotels.doctype.inn_shift.inn_shift.populate_cr_payment',
+		args: {
+			shift_id: shift_id
+		},
+		callback: (r) => {
+			if (r.message[0]) {
+				frm.set_value('cr_payment_transaction', []);
+				$.each(r.message[0], function (i, d) {
+					let item = frm.add_child('cr_payment_transaction');
+					item.type = d.type;
+					item.trx_id = d.trx_id;
+					item.reservation_id = d.reservation_id;
+					item.folio_id = d.folio_id;
+					item.customer_id = d.customer_id;
+					item.account = d.account;
+					item.amount = d.amount;
+					item.user = d.user;
+				});
+				frm.refresh_field('cr_payment_transaction');
+			}
+			if (r.message[1]) {
+				frm.set_value('payment_detail', []);
+				total_payment = 0;
+				$.each(r.message[1], function (i, d) {
+					let item = frm.add_child('payment_detail');
+					item.mode_of_payment = d.mode_of_payment;
+					item.amount = d.amount;
+					total_payment += d.amount;
+				});
+				frm.set_value('total_payment', total_payment);
+				console.log(r.message);
+				frm.refresh_field('payment_detail');
+			}
+		}
+	});
+	frappe.call({
+		method: 'inn.inn_hotels.doctype.inn_shift.inn_shift.populate_cr_refund',
+		args: {
+			shift_id: shift_id
+		},
+		callback: (r) => {
+			if (r.message[0]) {
+				frm.set_value('cr_refund_transaction', []);
+				$.each(r.message[0], function (i, d) {
+					let item = frm.add_child('cr_refund_transaction');
+					item.type = d.type;
+					item.trx_id = d.trx_id;
+					item.reservation_id = d.reservation_id;
+					item.folio_id = d.folio_id;
+					item.customer_id = d.customer_id;
+					item.account = d.account;
+					item.amount = d.amount;
+					item.user = d.user;
+				});
+				frm.refresh_field('cr_refund_transaction');
+			}
+			if (r.message[1]) {
+				frm.set_value('refund_detail', []);
+				total_refund = 0;
+				$.each(r.message[1], function (i, d) {
+					let item = frm.add_child('refund_detail');
+					item.type = d.type;
+					item.amount = d.amount;
+					total_refund += d.amount;
+				});
+				frm.set_value('total_refund', total_refund);
+				console.log(r.message);
+				frm.refresh_field('refund_detail');
+			}
+		}
+	});
+}
--- inn/inn_hotels/doctype/inn_shift/inn_shift.py
+++ inn/inn_hotels/doctype/inn_shift/inn_shift.py
@@ -15,3 +15,239 @@ def is_there_open_shift():
 		return 1
 	else:
 		return 2
+
+def get_last_closed_shift():
+	d = frappe.get_all('Inn Shift', filters={'status': 'Closed'}, order_by='creation desc', limit_page_length=1)
+	if d:
+		return frappe.get_doc('Inn Shift', d[0].name)
+	else:
+		return None
+
+@frappe.whitelist()
+def populate_cr_payment(shift_id):
+	returned_cr_payment_detail_list = []
+	cr_payment_detail_list = []
+	transaction_list = []
+	mode_of_payment = frappe.get_all('Mode of Payment')
+	reservation_list = frappe.get_all('Inn Reservation', filters={'status': ['in', ['In House', 'Finish']]}, fields=['*'])
+
+	if shift_id:
+		last_shift = get_last_closed_shift()
+		if last_shift is None:
+			for reservation_item in reservation_list:
+				folio_name = frappe.db.get_value('Inn Folio', {'reservation_id': reservation_item.name}, ['name'])
+				folio_transaction_list = frappe.get_all('Inn Folio Transaction',
+														filters={'parent': folio_name, 'flag': 'Credit', 'is_void': 0},
+														fields=['*'])
+				for folio_trx_item in folio_transaction_list:
+					payment_detail_doc = frappe.new_doc('Inn CR Payment Detail')
+					payment_detail_doc.mode_of_payment = folio_trx_item.mode_of_payment
+					payment_detail_doc.amount = folio_trx_item.amount
+					cr_payment_detail_list.append(payment_detail_doc)
+
+					payment_transaction_doc = frappe.new_doc('Inn CR Payment Transaction')
+					payment_transaction_doc.type = folio_trx_item.transaction_type
+					payment_transaction_doc.transaction_id = folio_trx_item.name
+					payment_transaction_doc.reservation_id = reservation_item.name
+					payment_transaction_doc.folio_id = folio_name
+					payment_transaction_doc.customer_id = reservation_item.customer_id
+					payment_transaction_doc.account = folio_trx_item.debit_account
+					payment_transaction_doc.amount = folio_trx_item.amount
+					payment_transaction_doc.user = payment_transaction_doc.owner
+					transaction_list.append(payment_transaction_doc)
+		else:
+			for reservation_item in reservation_list:
+				folio_name = frappe.db.get_value('Inn Folio', {'reservation_id': reservation_item.name}, ['name'])
+				folio_transaction_list = frappe.get_all('Inn Folio Transaction',
+														filters={'creation': ['>=', last_shift.time_out],
+																 'parent': folio_name,
+																 'flag': 'Credit',
+																 'is_void': 0},
+														fields=['*'])
+				for folio_trx_item in folio_transaction_list:
+					payment_detail_doc = frappe.new_doc('Inn CR Payment Detail')
+					payment_detail_doc.mode_of_payment = folio_trx_item.mode_of_payment
+					payment_detail_doc.amount = folio_trx_item.amount
+					cr_payment_detail_list.append(payment_detail_doc)
+
+					payment_transaction_doc = frappe.new_doc('Inn CR Payment Transaction')
+					payment_transaction_doc.type = folio_trx_item.transaction_type
+					payment_transaction_doc.transaction_id = folio_trx_item.name
+					payment_transaction_doc.reservation_id = reservation_item.name
+					payment_transaction_doc.folio_id = folio_name
+					payment_transaction_doc.customer_id = reservation_item.customer_id
+					payment_transaction_doc.account = folio_trx_item.debit_account
+					payment_transaction_doc.amount = folio_trx_item.amount
+					payment_transaction_doc.user = payment_transaction_doc.owner
+					transaction_list.append(payment_transaction_doc)
+	else:
+		if len(frappe.get_all('Inn Shift')) > 0:
+			last_shift = get_last_closed_shift()
+			for reservation_item in reservation_list:
+				folio_name = frappe.db.get_value('Inn Folio', {'reservation_id': reservation_item.name}, ['name'])
+				folio_transaction_list = frappe.get_all('Inn Folio Transaction',
+														filters={'creation': ['>=', last_shift.time_out],
+																 'parent': folio_name,
+																 'flag': 'Credit',
+																 'is_void': 0},
+														fields=['*'])
+				for folio_trx_item in folio_transaction_list:
+					payment_detail_doc = frappe.new_doc('Inn CR Payment Detail')
+					payment_detail_doc.mode_of_payment = folio_trx_item.mode_of_payment
+					payment_detail_doc.amount = folio_trx_item.amount
+					cr_payment_detail_list.append(payment_detail_doc)
+
+					payment_transaction_doc = frappe.new_doc('Inn CR Payment Transaction')
+					payment_transaction_doc.type = folio_trx_item.transaction_type
+					payment_transaction_doc.transaction_id = folio_trx_item.name
+					payment_transaction_doc.reservation_id = reservation_item.name
+					payment_transaction_doc.folio_id = folio_name
+					payment_transaction_doc.customer_id = reservation_item.customer_id
+					payment_transaction_doc.account = folio_trx_item.debit_account
+					payment_transaction_doc.amount = folio_trx_item.amount
+					payment_transaction_doc.user = payment_transaction_doc.owner
+					transaction_list.append(payment_transaction_doc)
+		else:
+			for reservation_item in reservation_list:
+				folio_name = frappe.db.get_value('Inn Folio', {'reservation_id': reservation_item.name}, ['name'])
+				folio_transaction_list = frappe.get_all('Inn Folio Transaction',
+														filters={'parent': folio_name, 'flag': 'Credit', 'is_void': 0},
+														fields=['*'])
+				for folio_trx_item in folio_transaction_list:
+					payment_detail_doc = frappe.new_doc('Inn CR Payment Detail')
+					payment_detail_doc.mode_of_payment = folio_trx_item.mode_of_payment
+					payment_detail_doc.amount = folio_trx_item.amount
+					cr_payment_detail_list.append(payment_detail_doc)
+
+					payment_transaction_doc = frappe.new_doc('Inn CR Payment Transaction')
+					payment_transaction_doc.type = folio_trx_item.transaction_type
+					payment_transaction_doc.transaction_id = folio_trx_item.name
+					payment_transaction_doc.reservation_id = reservation_item.name
+					payment_transaction_doc.folio_id = folio_name
+					payment_transaction_doc.customer_id = reservation_item.customer_id
+					payment_transaction_doc.account = folio_trx_item.debit_account
+					payment_transaction_doc.amount = folio_trx_item.amount
+					payment_transaction_doc.user = payment_transaction_doc.owner
+					transaction_list.append(payment_transaction_doc)
+
+	for mode_of_payment_item in mode_of_payment:
+		new_payment_detail = frappe.new_doc('Inn CR Payment Detail')
+		new_payment_detail.mode_of_payment = mode_of_payment_item.name
+		new_payment_detail.amount = 0
+		for cr_payment_detail_item in cr_payment_detail_list:
+			if cr_payment_detail_item.mode_of_payment == new_payment_detail.mode_of_payment:
+				new_payment_detail.amount += float(cr_payment_detail_item.amount)
+		if new_payment_detail.amount > 0:
+			returned_cr_payment_detail_list.append(new_payment_detail)
+
+
+	return transaction_list, returned_cr_payment_detail_list
+
+@frappe.whitelist()
+def populate_cr_refund(shift_id):
+	returned_cr_refund_detail_list = []
+	transaction_list = []
+	cr_refund = frappe.new_doc('Inn CR Refund Detail')
+	cr_refund.type = 'Refund'
+	cr_refund.amount = 0
+	reservation_list = frappe.get_all('Inn Reservation', filters={'status': ['in', ['In House', 'Finish']]},
+									  fields=['*'])
+
+	if shift_id:
+		last_shift = get_last_closed_shift()
+		if last_shift is None:
+			for reservation_item in reservation_list:
+				folio_name = frappe.db.get_value('Inn Folio', {'reservation_id': reservation_item.name}, ['name'])
+				folio_transaction_list = frappe.get_all('Inn Folio Transaction',
+														filters={'transaction_type': 'Refund',
+																 'parent': folio_name,
+																 'flag': 'Debit',
+																 'is_void': 0},
+														fields=['*'])
+				for folio_trx_item in folio_transaction_list:
+					cr_refund.amount += folio_trx_item.amount
+
+					refund_detail_doc = frappe.new_doc('Inn CR Refund Transaction')
+					refund_detail_doc.type = folio_trx_item.transaction_type
+					refund_detail_doc.transaction_id = folio_trx_item.name
+					refund_detail_doc.reservation_id = reservation_item.name
+					refund_detail_doc.folio_id = folio_name
+					refund_detail_doc.customer_id = reservation_item.customer_id
+					refund_detail_doc.account = folio_trx_item.credit_account
+					refund_detail_doc.amount = folio_trx_item.amount
+					refund_detail_doc.user = refund_detail_doc.owner
+					transaction_list.append(refund_detail_doc)
+		else:
+			for reservation_item in reservation_list:
+				folio_name = frappe.db.get_value('Inn Folio', {'reservation_id': reservation_item.name}, ['name'])
+				folio_transaction_list = frappe.get_all('Inn Folio Transaction',
+														filters={'transaction_type': 'Refund',
+																 'creation': ['>=', last_shift.time_out],
+																 'parent': folio_name,
+																 'flag': 'Debit',
+																 'is_void': 0},
+														fields=['*'])
+				for folio_trx_item in folio_transaction_list:
+					cr_refund.amount += folio_trx_item.amount
+
+					refund_detail_doc = frappe.new_doc('Inn CR Refund Transaction')
+					refund_detail_doc.type = folio_trx_item.transaction_type
+					refund_detail_doc.transaction_id = folio_trx_item.name
+					refund_detail_doc.reservation_id = reservation_item.name
+					refund_detail_doc.folio_id = folio_name
+					refund_detail_doc.customer_id = reservation_item.customer_id
+					refund_detail_doc.account = folio_trx_item.credit_account
+					refund_detail_doc.amount = folio_trx_item.amount
+					refund_detail_doc.user = refund_detail_doc.owner
+					transaction_list.append(refund_detail_doc)
+	else:
+		if len(frappe.get_all('Inn Shift')) > 0:
+			last_shift = get_last_closed_shift()
+			for reservation_item in reservation_list:
+				folio_name = frappe.db.get_value('Inn Folio', {'reservation_id': reservation_item.name}, ['name'])
+				folio_transaction_list = frappe.get_all('Inn Folio Transaction',
+														filters={'transaction_type': 'Refund',
+																 'creation': ['>=', last_shift.time_out],
+																 'parent': folio_name,
+																 'flag': 'Debit',
+																 'is_void': 0},
+														fields=['*'])
+				for folio_trx_item in folio_transaction_list:
+					cr_refund.amount += folio_trx_item.amount
+
+					refund_detail_doc = frappe.new_doc('Inn CR Refund Transaction')
+					refund_detail_doc.type = folio_trx_item.transaction_type
+					refund_detail_doc.transaction_id = folio_trx_item.name
+					refund_detail_doc.reservation_id = reservation_item.name
+					refund_detail_doc.folio_id = folio_name
+					refund_detail_doc.customer_id = reservation_item.customer_id
+					refund_detail_doc.account = folio_trx_item.credit_account
+					refund_detail_doc.amount = folio_trx_item.amount
+					refund_detail_doc.user = refund_detail_doc.owner
+					transaction_list.append(refund_detail_doc)
+		else:
+			for reservation_item in reservation_list:
+				folio_name = frappe.db.get_value('Inn Folio', {'reservation_id': reservation_item.name}, ['name'])
+				folio_transaction_list = frappe.get_all('Inn Folio Transaction',
+														filters={'transaction_type': 'Refund',
+																 'parent': folio_name,
+																 'flag': 'Debit',
+																 'is_void': 0},
+														fields=['*'])
+				for folio_trx_item in folio_transaction_list:
+					cr_refund.amount += folio_trx_item.amount
+
+					refund_detail_doc = frappe.new_doc('Inn CR Refund Transaction')
+					refund_detail_doc.type = folio_trx_item.transaction_type
+					refund_detail_doc.transaction_id = folio_trx_item.name
+					refund_detail_doc.reservation_id = reservation_item.name
+					refund_detail_doc.folio_id = folio_name
+					refund_detail_doc.customer_id = reservation_item.customer_id
+					refund_detail_doc.account = folio_trx_item.credit_account
+					refund_detail_doc.amount = folio_trx_item.amount
+					refund_detail_doc.user = refund_detail_doc.owner
+					transaction_list.append(refund_detail_doc)
+
+	returned_cr_refund_detail_list.append(cr_refund)
+
+	return transaction_list, returned_cr_refund_detail_list
