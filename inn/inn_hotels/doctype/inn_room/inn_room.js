deadf258f4e58b275eeb02a98745281ed0a09ff0
anra54 <anugrah.ramadhani@gmail.com>
2020-04-01 12:50:46 +0700

    quick change door status

--- inn/inn_hotels/doctype/inn_room/inn_room.js
+++ inn/inn_hotels/doctype/inn_room/inn_room.js
@@ -2,8 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Inn Room', {
-	refresh: function(frm) {
-
+	refresh: function(frm, cdt, cdn) {
+		frm.add_custom_button(__('Clear Door Status'), function () {
+			frappe.db.set_value(cdt, cdn, 'door_status', 'No Status');
+			frappe.show_alert('Door Status Cleared');
+		});
+		frm.add_custom_button(__('Do Not Disturb'), function () {
+			frappe.db.set_value(cdt, cdn, 'door_status', 'Do Not Disturb');
+			frappe.show_alert('Door Status set to Do Not Disturb');
+		});
+		frm.add_custom_button(__('Double Lock'), function () {
+			frappe.db.set_value(cdt, cdn, 'door_status', 'Double Lock');
+			frappe.show_alert('Door Status set to Double Lock');
+		});
+		frm.add_custom_button(__('Sleep Out'), function () {
+			frappe.db.set_value(cdt, cdn, 'door_status', 'Sleeping Out');
+			frappe.show_alert('Door Status set to Sleep Out');
+		});
 	},
 	amenities_template: function (frm) {
 		if (frm.doc.amenities_template) {
