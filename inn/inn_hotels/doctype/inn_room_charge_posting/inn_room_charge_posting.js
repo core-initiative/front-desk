f2fb386a1e5e3170b80a3741ac9ca485e69e3f69
anra54 <anugrah.ramadhani@gmail.com>
2020-04-07 22:38:54 +0700

    post  all & post individual  room charge

--- inn/inn_hotels/doctype/inn_room_charge_posting/inn_room_charge_posting.js
+++ inn/inn_hotels/doctype/inn_room_charge_posting/inn_room_charge_posting.js
@@ -2,7 +2,137 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Inn Room Charge Posting', {
-	refresh: function(frm) {
+	onload: function(frm) {
+		frm.get_field('tobe_posted').grid.cannot_add_rows = true;
+		frm.get_field('already_posted').grid.cannot_add_rows = true;
+		frm.get_field("tobe_posted").grid.only_sortable();
+		frm.get_field("already_posted").grid.only_sortable();
+		if (frm.doc.__islocal === 1) {
+			frappe.call({
+				method: 'inn.inn_hotels.doctype.inn_room_charge_posting.inn_room_charge_posting.is_there_open_room_charge_posting',
+				callback: (r) => {
+					if (r.message === 1) {
+						frappe.set_route('List', 'Inn Room Charge Posting');
+						frappe.msgprint('There are Room Charge Posting in progress. Please finish and close it first before opening new one.');
+					}
+				}
+			});
+		}
+	},
+	refresh: function (frm) {
+		if (frm.doc.status === 'Open') {
+			frappe.call({
+				method: 'inn.inn_hotels.doctype.inn_room_charge_posting.inn_room_charge_posting.get_posted_lists',
+				callback: (r) => {
+					if (r.message) {
+						if (r.message[0].length > 0) {
+							frm.set_value('tobe_posted', []);
+							$.each(r.message[0], function (i, d) {
+								var item = frm.add_child('tobe_posted');
+								item.reservation_id = d.reservation_id;
+								item.folio_id = d.folio_id;
+								item.room_id = d.room_id;
+								item.customer_id = d.customer_id;
+								item.room_rate_id = d.room_rate_id;
+								item.actual_room_rate = d.actual_room_rate;
+							});
+							frm.refresh_field('tobe_posted');
+						}
+						else{
+							frm.set_intro(__("There are no more Room Charge to be Posted. This Room Charge Posting can be Closed"));
+							frm.add_custom_button(__('Close'), function () {
+								frm.set_value('status', 'Closed');
+								frm.save();
+							});
+						}
+						if (r.message[1].length > 0) {
+							frm.set_value('already_posted',[]);
+							$.each(r.message[1], function (i, d) {
+								var item = frm.add_child('already_posted');
+								item.reservation_id = d.reservation_id;
+								item.folio_id = d.folio_id;
+								item.room_id = d.room_id;
+								item.customer_id = d.customer_id;
+								item.room_rate_id = d.room_rate_id;
+								item.actual_room_rate = d.actual_room_rate;
+								item.folio_transaction_id = d.folio_transaction_id;
+							});
+							frm.refresh_field('already_posted');
+						}
+					}
+				}
+			});
+		}
+		else {
+			frm.disable_save();
+			frm.set_df_property('sb2', 'hidden', 1);
+		}
+	},
+	post_individual_button: function(frm, cdt, cdn) {
+		if (frm.doc.tobe_posted.length > 0) {
+			let trx_selected = frm.get_field("tobe_posted").grid.get_selected();
+			if (trx_selected.length === 0) {
+				frappe.msgprint('There are no Room Charge selected to be posted.');
+			}
+			else {
+				let tobe_posted_list_individual = frm.doc.tobe_posted;
+				console.log(trx_selected);
+				for (let i = 0; i < tobe_posted_list_individual.length; i++) {
+					for(let j = 0; j < trx_selected.length; j++) {
+						if (tobe_posted_list_individual[i]['name'] !== trx_selected[j]) {
+							tobe_posted_list_individual.splice(i,1);
+						}
+					}
+				}
+				console.log(tobe_posted_list_individual);
+				frappe.confirm(__("You are about post the Room Charge. Are you sure?"), function () {
+					frappe.call({
+						method: 'inn.inn_hotels.doctype.inn_room_charge_posting.inn_room_charge_posting.post_room_charges',
+						args: {
+							tobe_posted_list: tobe_posted_list_individual,
+						},
+						callback: (r) => {
+							if (r.message) {
+								if ((tobe_posted_list_individual.length - trx_selected.length) === 0) { // post all but by selecting all then click post individual, doc status closed
+									console.log('masuk post individu like post all');
+									frm.reload_doc();
+									frappe.msgprint('All Room Charge successfully posted: <br> <ul>' + r.message + '</ul><br> This Room Charge Posting now can be Closed');
 
+								}
+								else { // post not all room charge, there are room charges left in tobe_posted, doc status not closed
+									console.log('masuk post individu but not like post all');
+									frm.reload_doc();
+									frappe.msgprint('Room Charge successfully posted: <br> <ul>' + r.message + '</ul>');
+								}
+							}
+						}
+					});
+				});
+			}
+		}
+		else {
+			frappe.msgprint('There are no Room Charge to be posted.');
+		}
+	},
+	post_all_button: function (frm, cdt, cdn) {
+		if (frm.doc.tobe_posted.length > 0) {
+			frappe.confirm(__("You are about post all of the Room Charges. Are you sure?"), function () {
+				frappe.call({
+					method: 'inn.inn_hotels.doctype.inn_room_charge_posting.inn_room_charge_posting.post_room_charges',
+					args: {
+						tobe_posted_list: frm.doc.tobe_posted,
+					},
+					callback: (r) => {
+						if (r.message) {
+							frm.reload_doc();
+							frappe.msgprint('All Room Charge successfully posted: <br> <ul>' + r.message + '</ul><br> This Room Charge Posting now can be Closed');
+						}
+					}
+				});
+			});
+		}
+		else {
+			frappe.msgprint('There are no Room Charge to be posted.');
+		}
 	}
 });
--- inn/inn_hotels/doctype/inn_room_charge_posting/inn_room_charge_posting.py
+++ inn/inn_hotels/doctype/inn_room_charge_posting/inn_room_charge_posting.py
@@ -3,8 +3,75 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+
+import json
 import frappe
+import datetime
 from frappe.model.document import Document
+from inn.inn_hotels.doctype.inn_folio_transaction_type.inn_folio_transaction_type import get_accounts_from_id
+from inn.inn_hotels.doctype.inn_folio_transaction.inn_folio_transaction import get_idx
 
 class InnRoomChargePosting(Document):
 	pass
+
+@frappe.whitelist()
+def is_there_open_room_charge_posting():
+	if frappe.get_all('Inn Room Charge Posting', {'status': 'Open'}):
+		return 1
+	else:
+		return 2
+
+@frappe.whitelist()
+def get_posted_lists():
+	tobe_posted_list = []
+	already_posted_list = []
+	folio_list = frappe.get_all('Inn Folio', filters={'status': 'Open', 'type': 'Guest'}, fields=['*'])
+	for item in folio_list:
+		reservation = frappe.get_doc('Inn Reservation', item.reservation_id)
+		room_charge_remark = 'Room Charge: ' + reservation.actual_room_id + " - " + datetime.datetime.today().strftime("%d-%m-%Y")
+		if frappe.db.exists('Inn Folio Transaction', {'parent': item.name, 'transaction_type': 'Room Charge', 'remark': room_charge_remark}):
+			folio_trx = frappe.get_doc('Inn Folio Transaction', {'parent': item.name, 'transaction_type': 'Room Charge',
+																 'remark': room_charge_remark})
+			posted = frappe.new_doc('Inn Room Charge Posted')
+			posted.reservation_id = item.reservation_id
+			posted.folio_id = item.name
+			posted.room_id = reservation.actual_room_id
+			posted.customer_id = reservation.customer_id
+			posted.room_rate_id = reservation.room_rate
+			posted.actual_room_rate = reservation.actual_room_rate
+			posted.folio_transaction_id = folio_trx.name
+			already_posted_list.append(posted)
+		else:
+			tobe_posted = frappe.new_doc('Inn Room Charge To Be Posted')
+			tobe_posted.reservation_id = item.reservation_id
+			tobe_posted.folio_id = item.name
+			tobe_posted.room_id = reservation.actual_room_id
+			tobe_posted.customer_id = reservation.customer_id
+			tobe_posted.room_rate_id = reservation.room_rate
+			tobe_posted.actual_room_rate = reservation.actual_room_rate
+			tobe_posted_list.append(tobe_posted)
+
+	return tobe_posted_list, already_posted_list
+
+@frappe.whitelist()
+def post_room_charges(tobe_posted_list):
+	return_value = ''
+	list_json = json.loads(tobe_posted_list)
+	for item in list_json:
+		debit_account, credit_account = get_accounts_from_id('Room Charge')
+		room_charge_folio_trx = frappe.new_doc('Inn Folio Transaction')
+		room_charge_folio_trx.flag = 'Debit'
+		room_charge_folio_trx.is_void = 0
+		room_charge_folio_trx.idx = get_idx(item['folio_id'])
+		room_charge_folio_trx.transaction_type = 'Room Charge'
+		room_charge_folio_trx.amount = item['actual_room_rate']
+		room_charge_folio_trx.debit_account = debit_account
+		room_charge_folio_trx.credit_account = credit_account
+		room_charge_folio_trx.remark = 'Room Charge: ' + item['room_id'] + " - " + datetime.datetime.today().strftime("%d-%m-%Y")
+		room_charge_folio_trx.parent = item['folio_id']
+		room_charge_folio_trx.parenttype = 'Inn Folio'
+		room_charge_folio_trx.parentfield = 'folio_transaction'
+		room_charge_folio_trx.insert()
+		return_value = return_value + '<li>' + room_charge_folio_trx.remark + '</li>'
+
+	return return_value
