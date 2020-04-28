6081fedb87680057f45332e64b469cc3a3299a8e
anra54 <anugrah.ramadhani@gmail.com>
2020-04-28 16:16:12 +0700

    filter folio & autofill other field when folio selected

--- inn/inn_hotels/doctype/ar_city_ledger/ar_city_ledger.py
+++ inn/inn_hotels/doctype/ar_city_ledger/ar_city_ledger.py
@@ -8,3 +8,29 @@ from frappe.model.document import Document
 
 class ARCityLedger(Document):
 	pass
+
+@frappe.whitelist()
+def get_folio_from_ar_city_ledger(selector = None, channel = None, group = None):
+	filters=[]
+	return_list = []
+	folio_list = []
+	if channel:
+		filters.append(['inn_channel_id', '=', channel])
+	if group:
+		filters.append('inn_group_id', '=', group)
+
+	for item in frappe.get_all('AR City Ledger', filters = filters, fields = ['*']):
+		return_list.append(item)
+		folio_list.append(item.folio_id)
+
+	if selector == 'Folio':
+		return folio_list
+	elif selector == 'AR City Ledger':
+		return return_list
+	else:
+		return return_list, folio_list
+
+@frappe.whitelist()
+def get_ar_city_ledger_by_folio(folio_id):
+	name =  frappe.db.get_value('AR City Ledger', {'folio_id': folio_id }, 'name')
+	return frappe.get_doc('AR City Ledger', name)
--- inn/inn_hotels/doctype/ar_city_ledger_invoice/ar_city_ledger_invoice.js
+++ inn/inn_hotels/doctype/ar_city_ledger_invoice/ar_city_ledger_invoice.js
@@ -3,6 +3,53 @@
 
 frappe.ui.form.on('AR City Ledger Invoice', {
 	refresh: function(frm) {
-
+		filter_folio(frm);
 	}
 });
+
+frappe.ui.form.on('AR City Ledger Invoice Folio', {
+	folio_id: function (frm, cdt, cdn) {
+		let child = locals[cdt][cdn];
+		get_id_by_folio(child);
+	}
+});
+
+function filter_folio(frm, channel = null, group = null) {
+	let field = frm.fields_dict['folio'].grid.fields_map['folio_id'];
+	frappe.call({
+		method: 'inn.inn_hotels.doctype.ar_city_ledger.ar_city_ledger.get_folio_from_ar_city_ledger',
+		args: {
+			selector: 'Folio',
+			channel: channel,
+			group: group
+		},
+		callback: (r) => {
+			if (r.message) {
+				field.get_query = function () {
+					return {
+						filters: [
+							['Inn Folio', 'name', 'in', r.message]
+						]
+					}
+				}
+			}
+		}
+	});
+}
+
+function get_id_by_folio(child) {
+	frappe.call({
+		method: 'inn.inn_hotels.doctype.ar_city_ledger.ar_city_ledger.get_ar_city_ledger_by_folio',
+		args: {
+			folio_id: child.folio_id
+		},
+		callback: (r) => {
+			child.customer_id = r.message.customer_id;
+			child.amount = r.message.total_amount;
+			child.open = r.message.folio_open;
+			child.close = r.message.folio_close;
+			child.ar_city_ledger_id = r.message.name;
+			cur_frm.refresh_field('folio');
+		}
+	});
+}
