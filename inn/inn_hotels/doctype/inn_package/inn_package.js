5f83a1c9b27e79b30763a3396fbd88ae11e70215
anra54 <anugrah.ramadhani@gmail.com>
2020-05-04 15:01:07 +0700

    calculate total amounts in package

--- inn/inn_hotels/doctype/inn_package/inn_package.js
+++ inn/inn_hotels/doctype/inn_package/inn_package.js
@@ -4,5 +4,33 @@
 frappe.ui.form.on('Inn Package', {
 	refresh: function(frm) {
 
+	},
+	amount_per_pax: function (frm) {
+		calculate_total_amounts(frm);
+	},
+	inn_tax_id: function (frm) {
+		calculate_total_amounts(frm);
+	},
+	total_pax: function (frm) {
+		calculate_total_amounts(frm);
 	}
 });
+
+function calculate_total_amounts(frm) {
+	if (frm.doc.amount_per_pax && frm.doc.inn_tax_id && frm.doc.total_pax) {
+		frappe.call({
+			method: 'inn.inn_hotels.doctype.inn_package.inn_package.calculate_amounts',
+			args: {
+				amount_per_pax: frm.doc.amount_per_pax,
+				tax_id: frm.doc.inn_tax_id,
+				total_pax: frm.doc.total_pax
+			},
+			callback: (r) => {
+				if (r.message[0]) {
+					frm.set_value('total_amount', r.message[0]);
+					frm.set_value('total_amount_after_tax', r.message[1]);
+				}
+			}
+		});
+	}
+}
--- inn/inn_hotels/doctype/inn_package/inn_package.py
+++ inn/inn_hotels/doctype/inn_package/inn_package.py
@@ -5,6 +5,14 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from inn.inn_hotels.doctype.inn_tax.inn_tax import calculate_inn_tax_and_charges
 
 class InnPackage(Document):
 	pass
+
+@frappe.whitelist()
+def calculate_amounts(amount_per_pax, tax_id, total_pax):
+	_, _, amount_per_pax_after_tax = calculate_inn_tax_and_charges(float(amount_per_pax), tax_id)
+	total_amount = float(amount_per_pax) * float(total_pax)
+	total_amount_after_tax = float(amount_per_pax_after_tax[-1]) * float(total_pax)
+	return total_amount, total_amount_after_tax
