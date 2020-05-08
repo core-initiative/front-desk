1e52120243aa9547161ac0d3384ad2986bf0a456
Fadhlan Hidayat <mail.fadhlanh@gmail.com>
2020-05-08 15:38:07 +0700

    update current date in pnl report

--- inn/inn_hotels/report/report_pnl/report_pnl.py
+++ inn/inn_hotels/report/report_pnl/report_pnl.py
@@ -59,99 +59,102 @@ def get_gl_entries(date, fiscal_year):
 
 def get_data(filters):
     data = []
+    
+    if filters.date and filters.fiscal_year:
+        date = datetime.datetime.strptime(filters.date, '%Y-%m-%d')
+        fiscal_year = int(filters.fiscal_year)
 
-    if filters.date and filters.fiscal_year and filters.date[:4] == filters.fiscal_year:
-        income_parent = {}
-        income_current_month = {}
-        income_last_month = {}
-        income_year_to_date = {}
+        if date.year == fiscal_year:
+            income_parent = {}
+            income_current_month = {}
+            income_last_month = {}
+            income_year_to_date = {}
 
-        accounts = get_accounts('Income')
-        for account in accounts:
-            if account[1]:
-                income_parent[account[0]] = account[1]
-            else:
-                income_parent[account[0]] = None
+            accounts = get_accounts('Income')
+            for account in accounts:
+                if account[1]:
+                    income_parent[account[0]] = account[1]
+                else:
+                    income_parent[account[0]] = None
 
-            income_current_month[account[0]] = 0
-            income_last_month[account[0]] = 0
-            income_year_to_date[account[0]] = 0
+                income_current_month[account[0]] = 0
+                income_last_month[account[0]] = 0
+                income_year_to_date[account[0]] = 0
 
-        expense_parent = {}
-        expense_current_month = {}
-        expense_last_month = {}
-        expense_year_to_date = {}
+            expense_parent = {}
+            expense_current_month = {}
+            expense_last_month = {}
+            expense_year_to_date = {}
 
-        accounts = get_accounts('Expense')
-        for account in accounts:
-            if account[1]:
-                expense_parent[account[0]] = account[1]
-            else:
-                expense_parent[account[0]] = None
+            accounts = get_accounts('Expense')
+            for account in accounts:
+                if account[1]:
+                    expense_parent[account[0]] = account[1]
+                else:
+                    expense_parent[account[0]] = None
 
-            expense_current_month[account[0]] = 0
-            expense_last_month[account[0]] = 0
-            expense_year_to_date[account[0]] = 0
+                expense_current_month[account[0]] = 0
+                expense_last_month[account[0]] = 0
+                expense_year_to_date[account[0]] = 0
 
-        month_now = datetime.datetime.now().month
 
-        gl_entries = get_gl_entries(filters.date, filters.fiscal_year)
-        for gl_entry in gl_entries:
-            account = gl_entry[1]
+            gl_entries = get_gl_entries(date, fiscal_year)
+            for gl_entry in gl_entries:
+                account = gl_entry[1]
 
-            if account[:1] == '4':
-                income_year_to_date[account] = income_year_to_date[account] + gl_entry[2] - gl_entry[3]
-
-                if gl_entry[0].month == month_now:
-                    income_current_month[account] = income_current_month[account] + gl_entry[2] - gl_entry[3]
-                elif gl_entry[0].month+1 == month_now:
-                    income_last_month[account] = income_last_month[account] + gl_entry[2] - gl_entry[3]
-
-                while income_parent[account] is not None:
-                    account = income_parent[account]
-
+                if account[:1] == '4':
                     income_year_to_date[account] = income_year_to_date[account] + gl_entry[2] - gl_entry[3]
 
-                    if gl_entry[0].month == month_now:
+                    if gl_entry[0].month == date.month:
                         income_current_month[account] = income_current_month[account] + gl_entry[2] - gl_entry[3]
-                    elif gl_entry[0].month+1 == month_now:
+                    elif gl_entry[0].month+1 == date.month:
                         income_last_month[account] = income_last_month[account] + gl_entry[2] - gl_entry[3]
 
-            elif account[:1] == '5':
-                expense_year_to_date[account] = expense_year_to_date[account] - gl_entry[2] + gl_entry[3]
+                    while income_parent[account] is not None:
+                        account = income_parent[account]
 
-                if gl_entry[0].month == month_now:
-                    expense_current_month[account] = expense_current_month[account] - gl_entry[2] + gl_entry[3]
-                elif gl_entry[0].month+1 == month_now:
-                    expense_last_month[account] = expense_last_month[account] - gl_entry[2] + gl_entry[3]
+                        income_year_to_date[account] = income_year_to_date[account] + gl_entry[2] - gl_entry[3]
 
-                while expense_parent[account] is not None:
-                    account = expense_parent[account]
+                        if gl_entry[0].month == date.month:
+                            income_current_month[account] = income_current_month[account] + gl_entry[2] - gl_entry[3]
+                        elif gl_entry[0].month+1 == date.month:
+                            income_last_month[account] = income_last_month[account] + gl_entry[2] - gl_entry[3]
 
+                elif account[:1] == '5':
                     expense_year_to_date[account] = expense_year_to_date[account] - gl_entry[2] + gl_entry[3]
 
-                    if gl_entry[0].month == month_now:
+                    if gl_entry[0].month == date.month:
                         expense_current_month[account] = expense_current_month[account] - gl_entry[2] + gl_entry[3]
-                    elif gl_entry[0].month+1 == month_now:
+                    elif gl_entry[0].month+1 == date.month:
                         expense_last_month[account] = expense_last_month[account] - gl_entry[2] + gl_entry[3]
-                
-        for item in income_parent:
-            data.append({
-                    'account': item,
-                    'current_month': income_current_month[item],
-                    'last_month': income_last_month[item],
-                    'year_to_date': income_year_to_date[item]
-                })
 
-        for item in expense_parent:
-            data.append({
-                    'account': item,
-                    'current_month': expense_current_month[item],
-                    'last_month': expense_last_month[item],
-                    'year_to_date': expense_year_to_date[item]
-                })
-            
-        data = json.dumps(data)
-        data = ast.literal_eval(data)
+                    while expense_parent[account] is not None:
+                        account = expense_parent[account]
+
+                        expense_year_to_date[account] = expense_year_to_date[account] - gl_entry[2] + gl_entry[3]
+
+                        if gl_entry[0].month == date.month:
+                            expense_current_month[account] = expense_current_month[account] - gl_entry[2] + gl_entry[3]
+                        elif gl_entry[0].month+1 == date.month:
+                            expense_last_month[account] = expense_last_month[account] - gl_entry[2] + gl_entry[3]
+                    
+            for item in income_parent:
+                data.append({
+                        'account': item,
+                        'current_month': income_current_month[item],
+                        'last_month': income_last_month[item],
+                        'year_to_date': income_year_to_date[item]
+                    })
+
+            for item in expense_parent:
+                data.append({
+                        'account': item,
+                        'current_month': expense_current_month[item],
+                        'last_month': expense_last_month[item],
+                        'year_to_date': expense_year_to_date[item]
+                    })
+                
+            data = json.dumps(data)
+            data = ast.literal_eval(data)
 
     return data
