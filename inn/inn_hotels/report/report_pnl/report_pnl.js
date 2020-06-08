6f1cb2fde9e3ef7b081535883c900239b01d6b65
Fadhlan Hidayat <mail.fadhlanh@gmail.com>
2020-06-08 22:27:04 +0700

    update report pnl

--- inn/inn_hotels/report/report_pnl/report_pnl.js
+++ inn/inn_hotels/report/report_pnl/report_pnl.js
@@ -16,4 +16,29 @@ frappe.query_reports["Report PNL"] = {
 			options: 'Fiscal Year'
 		},
 	],
+	"formatter": function(value, row, column, data, default_formatter) {
+		if (column.fieldname=="account") {
+			value = data.account;
+			column.is_tree = true;
+		}
+
+		value = default_formatter(value, row, column, data);
+
+		if (!data.parent_account) {
+			value = $(`<span>${value}</span>`);
+
+			var $value = $(value).css("font-weight", "bold");
+			if (data.warn_if_negative && data[column.fieldname] < 0) {
+				$value.addClass("text-danger");
+			}
+
+			value = $value.wrap("<p></p>").parent().html();
+		}
+
+		return value;
+	},
+	"tree": true,
+	"name_field": "account",
+	"parent_field": "parent_account",
+	"initial_depth": 10,
 }
