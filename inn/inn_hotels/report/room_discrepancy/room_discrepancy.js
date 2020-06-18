716f41b2fca6fdf69c6edea235bb3d8fcd23722f
Fadhlan Hidayat <mail.fadhlanh@gmail.com>
2020-06-18 09:38:55 +0700

    F.8. Print Room Discrepancy

---
+++ inn/inn_hotels/report/room_discrepancy/room_discrepancy.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Core Initiative and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Room Discrepancy"] = {
+	"filters": [
+
+	]
+}
