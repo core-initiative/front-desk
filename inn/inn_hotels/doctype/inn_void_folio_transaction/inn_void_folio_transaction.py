236f7094f88bf331d70794ce722f5761e280bb9f
anra54 <anugrah.ramadhani@gmail.com>
2020-07-24 16:36:31 +0700

    respond to request works in bundle fashion

--- inn/inn_hotels/doctype/inn_void_folio_transaction/inn_void_folio_transaction.py
+++ inn/inn_hotels/doctype/inn_void_folio_transaction/inn_void_folio_transaction.py
@@ -5,13 +5,38 @@
 from __future__ import unicode_literals
 import frappe
 import datetime
+from inn.inn_hotels.doctype.inn_folio_transaction_bundle.inn_folio_transaction_bundle import get_trx_list
 from frappe.model.document import Document
 
 class InnVoidFolioTransaction(Document):
 	pass
 
 @frappe.whitelist()
-def respond_void(id, response, denied_reason=None):
+def respond_void(id, response, bundle_len, denied_reason=None):
+	if int(bundle_len) > 1:
+		list = get_trx_list(frappe.get_doc('Inn Void Folio Transaction', id).folio_transaction_id)
+		for item in list:
+			trx_doc = frappe.get_doc('Inn Folio Transaction', item.name)
+			respond_single_void_request(trx_doc.void_id, response, denied_reason)
+
+		if response == 'Approved':
+			return 1
+		elif response == 'Denied':
+			return 0
+
+	else:
+		response_back = respond_single_void_request(id, response, denied_reason)
+
+		if response_back == 'Approved':
+			return 1
+		elif response_back == 'Denied':
+			return 0
+
+@frappe.whitelist()
+def request_status(id):
+	return frappe.db.get_value('Inn Void Folio Transaction', id, 'status')
+
+def respond_single_void_request(id, response, denied_reason):
 	doc = frappe.get_doc('Inn Void Folio Transaction', id)
 	doc.status = response
 	doc.denied_reason = denied_reason
@@ -26,11 +51,4 @@ def respond_void(id, response, denied_reason=None):
 						 "\n This transaction is VOIDED. Details in Inn Void Folio Transaction: " + \
 						 doc.name
 		trx_doc.save()
-
-		return 1
-	elif doc.status == 'Denied':
-		return 0
-
-@frappe.whitelist()
-def request_status(id):
-	return frappe.db.get_value('Inn Void Folio Transaction', id, 'status')
+	return doc.status
