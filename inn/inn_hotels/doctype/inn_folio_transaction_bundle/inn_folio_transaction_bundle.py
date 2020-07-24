2a9c85c414c057f10c78bbbbb5852876166b31ab
anra54 <anugrah.ramadhani@gmail.com>
2020-07-24 14:23:42 +0700

    get_trx_list function

--- inn/inn_hotels/doctype/inn_folio_transaction_bundle/inn_folio_transaction_bundle.py
+++ inn/inn_hotels/doctype/inn_folio_transaction_bundle/inn_folio_transaction_bundle.py
@@ -8,3 +8,13 @@ from frappe.model.document import Document
 
 class InnFolioTransactionBundle(Document):
 	pass
+
+@frappe.whitelist()
+def get_trx_list(bundle_id, len_only=False):
+	if bundle_id:
+		if len_only:
+			return len(frappe.get_all('Inn Folio Transaction', filters={'ftb_id': bundle_id}))
+		else:
+			return frappe.get_all('Inn Folio Transaction', filters={'ftb_id': bundle_id})
+	else:
+		return None
