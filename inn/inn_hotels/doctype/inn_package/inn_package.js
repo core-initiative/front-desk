f86135a6b16e9ecd57dc68367b479cf470831c8f
anra54 <anugrah.ramadhani@gmail.com>
2021-03-22 17:37:07 +0700

    inn package input harga dengan harga setelah tax

--- inn/inn_hotels/doctype/inn_package/inn_package.js
+++ inn/inn_hotels/doctype/inn_package/inn_package.js
@@ -5,32 +5,33 @@ frappe.ui.form.on('Inn Package', {
 	refresh: function(frm) {
 
 	},
-	amount_per_pax: function (frm) {
-		calculate_total_amounts(frm);
+	total_pax: function (frm) {
+		calculate_amount_before_tax(frm);
 	},
 	inn_tax_id: function (frm) {
-		calculate_total_amounts(frm);
+		calculate_amount_before_tax(frm);
 	},
-	total_pax: function (frm) {
-		calculate_total_amounts(frm);
+	total_amount_after_tax: function (frm) {
+		calculate_amount_before_tax(frm);
 	}
 });
 
-function calculate_total_amounts(frm) {
-	if (frm.doc.amount_per_pax && frm.doc.inn_tax_id && frm.doc.total_pax) {
+function calculate_amount_before_tax(frm) {
+	if (frm.doc.total_amount_after_tax && frm.doc.inn_tax_id && frm.doc.total_pax) {
 		frappe.call({
-			method: 'inn.inn_hotels.doctype.inn_package.inn_package.calculate_amounts',
+			method: 'inn.inn_hotels.doctype.inn_package.inn_package.calculate_amounts_before_tax',
 			args: {
-				amount_per_pax: frm.doc.amount_per_pax,
+				amount_after_tax: frm.doc.total_amount_after_tax,
 				tax_id: frm.doc.inn_tax_id,
 				total_pax: frm.doc.total_pax
 			},
 			callback: (r) => {
-				if (r.message[0]) {
+				if (r.message) {
+					console.log(r.message);
 					frm.set_value('total_amount', r.message[0]);
-					frm.set_value('total_amount_after_tax', r.message[1]);
+					frm.set_value('amount_per_pax', r.message[1]);
 				}
 			}
-		});
+		})
 	}
 }
--- inn/inn_hotels/doctype/inn_package/inn_package.py
+++ inn/inn_hotels/doctype/inn_package/inn_package.py
@@ -3,6 +3,8 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+
+import math
 import frappe
 from frappe.model.document import Document
 from inn.inn_hotels.doctype.inn_tax.inn_tax import calculate_inn_tax_and_charges
@@ -15,6 +17,33 @@ def calculate_amounts(amount_per_pax, tax_id, total_pax):
 	total_amount = float(amount_per_pax) * float(total_pax)
 	_, _, total_amount_after_tax = calculate_inn_tax_and_charges(total_amount, tax_id)
 	return total_amount, total_amount_after_tax[-1]
+
+@frappe.whitelist()
+def calculate_amounts_before_tax(amount_after_tax, tax_id, total_pax):
+	tax_breakdown_list = frappe.get_all('Inn Tax Breakdown',
+												  filters={'parent': tax_id},
+												  order_by="idx desc",
+												  fields=['*'])
+
+	if len(tax_breakdown_list) > 0:
+		tax_id = [""] * len(tax_breakdown_list)
+		rate_before = [0] * len(tax_breakdown_list)
+		rate_after = [0] * len(tax_breakdown_list)
+		for index, item in enumerate(tax_breakdown_list):
+			tax_id[index] = item.breakdown_type
+			if item.breakdown_type == 'On Net Total':
+				denominator = (100 + float(item.breakdown_rate)) / 100
+				if index == 0:
+					rate_before[index] = math.floor(float(amount_after_tax)/denominator)
+					rate_after[index] = int(amount_after_tax)
+				else:
+					rate_before[index] = math.floor(rate_before[index-1]/denominator)
+					rate_after[index] = rate_before[index-1]
+		rate_before_per_pax = float(rate_before[-1])/float(total_pax)
+
+		return rate_before[-1], rate_before_per_pax
+	else:
+		frappe.msgprint("Tax Breakdown in " + tax_id + "are not defined. Please define it first.")
 
 @frappe.whitelist()
 def get_package_list(active_flag=None):
