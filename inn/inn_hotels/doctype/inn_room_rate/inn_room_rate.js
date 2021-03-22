9780cd50048bf15d864fb342cc9872adea1a3719
anra54 <anugrah.ramadhani@gmail.com>
2021-03-22 14:08:56 +0700

    input room rate dengan harga final after tax

--- inn/inn_hotels/doctype/inn_room_rate/inn_room_rate.js
+++ inn/inn_hotels/doctype/inn_room_rate/inn_room_rate.js
@@ -2,7 +2,20 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Inn Room Rate', {
-	refresh: function(frm) {
-
+	final_total_rate_amount: function (frm) {
+		if (frm.doc.final_breakfast_rate_amount >= 0) {
+			if (frm.doc.final_breakfast_rate_amount >= frm.doc.final_total_rate_amount) {
+				frm.set_value('final_total_rate_amount', 0);
+				frappe.msgprint("Breakfast Rate must be less than Total Rate.");
+				frappe.validated = false;
+			}
+		}
+	},
+	final_breakfast_rate_amount: function(frm) {
+		if (frm.doc.final_breakfast_rate_amount >= frm.doc.final_total_rate_amount) {
+			frappe.msgprint("Breakfast Rate must be less than Total Rate.");
+			frm.set_value('final_breakfast_rate_amount', 0);
+			frappe.validated = false;
+		}
 	}
 });
--- inn/inn_hotels/doctype/inn_room_rate/inn_room_rate.py
+++ inn/inn_hotels/doctype/inn_room_rate/inn_room_rate.py
@@ -12,14 +12,15 @@ from inn.inn_hotels.doctype.inn_tax.inn_tax import calculate_inn_tax_and_charges
 class InnRoomRate(Document):
 	pass
 
-
 def calculate_total_amount(doc, method):
-	_, _, room_rate_after_tax = calculate_inn_tax_and_charges(float(doc.room_rate), doc.room_rate_tax)
-	_, _, breakfast_rate_after_tax = calculate_inn_tax_and_charges(float(doc.breakfast_rate), doc.breakfast_tax)
+	final_room_rate_only_amount = doc.final_total_rate_amount - doc.final_breakfast_rate_amount
+	_, room_rate_before, _, _, bf_rate_before, _ = calculate_rate_breakdown_from_final_rate(
+		doc.room_rate_tax, doc.breakfast_tax, final_room_rate_only_amount, doc.final_breakfast_rate_amount)
 
-	doc.total_rate = doc.room_rate + doc.breakfast_rate
-	doc.rate_after_tax = math.ceil(float(room_rate_after_tax[-1]) + float(breakfast_rate_after_tax[-1]))
-
+	doc.total_rate = float(room_rate_before[-1]) + float(bf_rate_before[-1])
+	doc.rate_after_tax = doc.final_total_rate_amount
+	doc.room_rate = float(room_rate_before[-1])
+	doc.breakfast_rate = float(bf_rate_before[-1])
 
 def get_room_rate_after_tax(room_rate_id):
 	doc = frappe.get_doc('Inn Room Rate', room_rate_id)
@@ -52,19 +53,19 @@ def get_actual_room_rate_before_from_actual_rate(room_rate_id, actual_rate):
 			if item.breakdown_type == 'On Net Total':
 				denominator = (100 + float(item.breakdown_rate))/100
 				if index == 0:
-					tb_rate_before[index] = math.ceil(float(actual_room_rate)/denominator)
+					tb_rate_before[index] = math.floor(float(actual_room_rate)/denominator)
 					tb_rate_after[index] = actual_room_rate
 				else:
-					tb_rate_before[index] = math.ceil(float(tb_rate_before[index-1])/denominator)
+					tb_rate_before[index] = math.floor(float(tb_rate_before[index-1])/denominator)
 					tb_rate_after[index] = tb_rate_after[index-1]
 			elif item.breakdown_type == 'On Previous Row Total':
 				denominator = (100 + float(item.breakdown_rate)) / 100
 				if index < len(room_rate_tax_breakdown_list):
 					if index == 0:
-						tb_rate_after[len(room_rate_tax_breakdown_list) - int(item.breakdown_row_id)-1] = math.ceil(float(actual_room_rate)/denominator)
+						tb_rate_after[len(room_rate_tax_breakdown_list) - int(item.breakdown_row_id)-1] = math.floor(float(actual_room_rate)/denominator)
 						tb_rate_before[index] = float(actual_room_rate)/denominator
 					else:
-						tb_rate_after[len(room_rate_tax_breakdown_list) - int(item.breakdown_row_id)-1] = math.ceil(float(tb_rate_after[index-1])/denominator)
+						tb_rate_after[len(room_rate_tax_breakdown_list) - int(item.breakdown_row_id)-1] = math.floor(float(tb_rate_after[index-1])/denominator)
 						tb_rate_before[index] = float(tb_rate_after[index-1])/denominator
 		return tb_rate_before[-1]
 	else:
@@ -85,3 +86,53 @@ def get_actual_room_rate_breakdown(room_rate_id, actual_rate):
 	breakfast_rate = room_rate_doc.breakfast_rate
 
 	return room_rate_tax, room_rate, breakfast_tax, breakfast_rate
+
+@frappe.whitelist()
+def calculate_rate_breakdown_from_final_rate(room_rate_tax_name, breakfast_tax_name, final_room_rate_amount, final_breakfast_rate_amount):
+	# Calculate for room rate
+	room_rate_tax_breakdown_list = frappe.get_all('Inn Tax Breakdown',
+												  filters={'parent': room_rate_tax_name},
+												  order_by="idx desc",
+												  fields=['*'])
+
+	if len(room_rate_tax_breakdown_list) > 0 :
+		room_tax_id = [""] * len(room_rate_tax_breakdown_list)
+		room_rate_before = [0] * len(room_rate_tax_breakdown_list)
+		room_rate_after = [0] * len(room_rate_tax_breakdown_list)
+		for index, item in enumerate(room_rate_tax_breakdown_list):
+			room_tax_id[index] = item.breakdown_type
+			if item.breakdown_type == 'On Net Total':
+				denominator = (100 + float(item.breakdown_rate)) / 100
+				if index == 0:
+					room_rate_before[index] = math.floor(float(final_room_rate_amount)/denominator)
+					room_rate_after[index] = int(final_room_rate_amount)
+				else:
+					room_rate_before[index] = math.floor(float(room_rate_before[index-1])/denominator)
+					room_rate_after[index] = room_rate_before[index-1]
+	else:
+		frappe.msgprint("Tax Breakdown in " + room_rate_tax_name + " are not defined. Please define it first.")
+
+	# Calculate for breakfast rate
+	breakfast_rate_tax_breakdown_list = frappe.get_all('Inn Tax Breakdown',
+													   filters={'parent': breakfast_tax_name},
+													   order_by="idx desc",
+													   fields=['*'])
+	if len(breakfast_rate_tax_breakdown_list) > 0:
+		bf_tax_id = [""] * len(breakfast_rate_tax_breakdown_list)
+		bf_rate_before = [0] * len(breakfast_rate_tax_breakdown_list)
+		bf_rate_after = [0] * len(breakfast_rate_tax_breakdown_list)
+		for index, item in enumerate(breakfast_rate_tax_breakdown_list):
+			bf_tax_id[index] = item.breakdown_type
+			if item.breakdown_type == 'On Net Total':
+				denominator = (100 + float(item.breakdown_rate)) / 100
+				if index == 0:
+					bf_rate_before[index] = math.floor(float(final_breakfast_rate_amount)/denominator)
+					bf_rate_after[index] = int(final_breakfast_rate_amount)
+				else:
+					bf_rate_before[index] = math.floor(float(bf_rate_before[index-1])/denominator)
+					bf_rate_after[index] = bf_rate_before[index-1]
+	else:
+		frappe.msgprint("Tax Breakdown in " + breakfast_tax_name + " are not defined. Please define it first.")
+
+	return room_tax_id, room_rate_before, room_rate_after, bf_tax_id, bf_rate_before, bf_rate_after
+
