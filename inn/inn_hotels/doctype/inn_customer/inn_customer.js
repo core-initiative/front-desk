1d43fc65a7309caa439390642eb83bdda1017804
anhilmy <hilmyahmadnaufal@gmail.com>
2024-02-23 15:13:42 +0700

    fix; customer and channe;

--- inn/inn_hotels/doctype/inn_customer/inn_customer.js
+++ inn/inn_hotels/doctype/inn_customer/inn_customer.js
@@ -11,6 +11,7 @@ function fieldToggle(frm) {
     console.log(frm.doc.__islocal)
     if (frm.doc.__islocal === 1) {
         frm.set_df_property('supplier_name', 'hidden', 1);
+        frm.set_df_property('customer', 'hidden', 1);
     } else {
         frm.set_df_property('supplier_name', 'read_only', 1);
         frm.set_df_property('customer_name', 'read_only', 1);
