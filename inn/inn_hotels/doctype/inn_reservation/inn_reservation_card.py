2c2d1fecf28d271182bef6d5a955166459f3f4e9
anhilmy <hilmyahmadnaufal@gmail.com>
2024-03-01 14:41:46 +0700

    fix: card owner semantics

--- inn/inn_hotels/doctype/inn_reservation/inn_reservation_card.py
+++ inn/inn_hotels/doctype/inn_reservation/inn_reservation_card.py
@@ -5,7 +5,7 @@ from datetime import date
 @frappe.whitelist()
 def get_card_number_expected_departure():
     today = date.today().isoformat()
-    data = frappe.db.count("Inn Reservation", {"status": ["!=", "Finish"], "expected_departure": today})
+    data = frappe.db.count("Inn Reservation", {"status": ["=", "In House"], "expected_departure": today})
 
     result = {
         "value": data,
@@ -20,12 +20,26 @@ def get_card_number_expected_departure():
 @frappe.whitelist()
 def get_card_number_expected_arrival():
     today = date.today().isoformat()
-    data = frappe.db.count("Inn Reservation", {"status": ["!=", "In House"], "expected_arrival": today})
+    data = frappe.db.count("Inn Reservation", {"status": ["=", "Reserved"], "expected_arrival": today})
 
     result = {
         "value": data,
         "fieldtype": "Int",
-        "route_options": {"expected_arrival": today, "status": ["!=", "In House"]},
+        "route_options": {"expected_arrival": today, "status": ["=", "Reserved"]},
+        "route": ["inn-reservation"]
+    }
+
+    return result
+
+@frappe.whitelist()
+def get_card_number_check_out():
+    today = date.today().isoformat()
+    data = frappe.db.count("Inn Reservation", {"status": ["=", "Finish"], "expected_departure": today})
+
+    result = {
+        "value": data,
+        "fieldtype": "Int",
+        "route_options": {"expected_departure": today, "status": ["=", "Finish"]},
         "route": ["inn-reservation"]
     }
 
