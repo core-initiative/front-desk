35e89016c07fea67f92a7ca1e3b9b1923760d242
anhilmy <hilmyahmadnaufal@gmail.com>
2024-03-22 15:17:28 +0700

    feat: add flow transfer to folio
    add button transfer to folio, select inn folio to be transferred
    add row to inn folio with charge, service, tax, and roundoff
    remove number from pos invoice and payments

--- inn/public/js/inn-pos.bundle.js
+++ inn/public/js/inn-pos.bundle.js
@@ -1,3 +1,5 @@
 import "../../inn_hotels/page/pos_extended/pos_extended_itemcart.js";
 import "../../inn_hotels/page/pos_extended/pos_extended_pastordersummary.js";
 import "../../inn_hotels/page/pos_extended/pos_extended_printformat.js";
+import "../../inn_hotels/page/pos_extended/pos_extended_payment.js";
+import "../../inn_hotels/page/pos_extended/pos_extended_pastorderlist.js";
