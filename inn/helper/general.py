3c3902e7ceabdb8ea688899f6f09f9b3b3e6b729
anhilmy <hilmyahmadnaufal@gmail.com>
2024-04-05 14:54:21 +0700

    fix: company default endpint

--- inn/helper/general.py
+++ inn/helper/general.py
@@ -1,6 +1,21 @@
-
+from werkzeug.wrappers import Response
+import json
 import frappe
 
 @frappe.whitelist()
 def get_role():
     return frappe.get_roles(frappe.session.user)
+
+
+@frappe.whitelist()
+def get_default_company():
+    company = frappe.db.get_single_value("Global Defaults", "default_company")
+    data = {
+        "data" : {
+            "default_company": company
+        }
+    }
+    response = Response()
+    response.data = json.dumps(data)
+    response.headers["Content-Type"] = "application/json"
+    return response
