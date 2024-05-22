1da865b642eded7afd54dbe349a5c2b6ee39386c
anhilmy <hilmyahmadnaufal@gmail.com>
2024-05-22 15:50:06 +0700

    fix; display table order choice

--- inn/inn_hotels/page/pos_extended/pos_extended_itemcart.js
+++ inn/inn_hotels/page/pos_extended/pos_extended_itemcart.js
@@ -17,6 +17,42 @@ frappe.require(["point-of-sale.bundle.js", "inn-pos.bundle.js"], function () {
             this.make_table_selector();
         }
 
+        update_table_section() {
+            const me = this;
+            if (me.table_number) {
+                this.$table_section.html(`
+                <div style="
+                    display: flex;
+                    align-items: center;
+                    font-weight: 700;
+                    font-size: var(--text-lg);    
+                ">
+                    <div style="
+                    width: 3rem;
+                    margin-right: var(--margin-md);
+                    ">
+                    </div>
+
+                    <div style="
+                        flex: auto;
+                    ">
+                        <div>
+                            <span>Table ${me.table_number}</span>
+                        </div>
+                    </div>
+
+                    <div class="reset-table-btn" style="cursor: pointer;">
+                        <svg width="32" height="32" viewBox="0 0 14 14" fill="none">
+                            <path d="M4.93764 4.93759L7.00003 6.99998M9.06243 9.06238L7.00003 6.99998M7.00003 6.99998L4.93764 9.06238L9.06243 4.93759" stroke="#8D99A6"/>
+                        </svg>
+                    </div>
+                </div>
+                `)
+            } else {
+                this.make_table_selector()
+            }
+        }
+
 
         make_table_selector() {
             this.$table_section.html(`<div class="table-field"></div>`)
@@ -37,6 +73,7 @@ frappe.require(["point-of-sale.bundle.js", "inn-pos.bundle.js"], function () {
                     onchange: function () {
                         me["table_number"] = this.get_value()
                         me.events.get_frm().dirty()
+                        me.update_table_section()
                     }
                 },
                 parent: this.$table_section.find('.table-field'),
@@ -44,6 +81,7 @@ frappe.require(["point-of-sale.bundle.js", "inn-pos.bundle.js"], function () {
             });
             this.table_field.toggle_label(false);
         }
+
         load_invoice() {
             super.load_invoice()
 
@@ -61,10 +99,12 @@ frappe.require(["point-of-sale.bundle.js", "inn-pos.bundle.js"], function () {
                     table_number = r.message
                     if (table_number) {
                         me.$table_section.find("input").val(table_number.table)
-                        me.table_number = table_number
+                        me.table_number = table_number.table
+                        me.update_table_section()
                     } else {
                         me.$table_section.find("input").val()
                         me.table_number = ""
+                        me.update_table_section()
                     }
                 }
             })
@@ -127,6 +167,11 @@ frappe.require(["point-of-sale.bundle.js", "inn-pos.bundle.js"], function () {
             }
         }
 
+        reset_table_section() {
+            this["table_number"] = undefined
+            this.make_table_selector()
+        }
+
         bind_events() {
             super.bind_events()
 
@@ -147,6 +192,10 @@ frappe.require(["point-of-sale.bundle.js", "inn-pos.bundle.js"], function () {
                 if ($(this).attr('style').indexOf('--gray-800') == -1) return;
 
                 await me.events.transfer_folio();
+            })
+
+            this.$component.on("click", ".reset-table-btn", async function () {
+                me.reset_table_section()
             })
         }
     }
