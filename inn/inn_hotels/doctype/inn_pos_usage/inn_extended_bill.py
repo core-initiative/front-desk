fccdf962e6e85fffd277776eb9db45108e8c6fe8
anhilmy <hilmyahmadnaufal@gmail.com>
2024-07-23 15:39:43 +0700

    fix: operational bug
    - show table (or room) in pos bill and pos invoice
    - fix grand total showing 0 in transferred pos invoice
    - extend dropship purchase order print format

--- inn/inn_hotels/doctype/inn_pos_usage/inn_extended_bill.py
+++ inn/inn_hotels/doctype/inn_pos_usage/inn_extended_bill.py
@@ -8,22 +8,26 @@ def extended_bil_extra_data(invoice):
         address = frappe.get_all("Dynamic Link", filters={"link_doctype": "Company", "link_name": invoice.company,"parenttype":"Address"}, fields=["parent"])
         address = frappe.get_value("Address", filters=address[0].parent, fieldname="address_line1", as_dict=True)
         address = address.address_line1
-    
-    order_id = frappe.get_value("Inn POS Usage", filters={"pos_invoice": invoice.name}, fieldname="name", as_dict=True)
+    order_id, transferred_to, table = frappe.get_value("Inn POS Usage", filters={"pos_invoice": invoice.name}, fieldname=["name", "transfer_to_folio", "table"])
     if order_id == None:
         order_id = ""
-    else:
-        order_id = order_id.name
 
     total_tax = 0
     for tax in invoice.taxes:
         total_tax += tax.tax_amount
-    total_tax = utils.fmt_money(total_tax, 2, "Rp")
     tax_name = frappe.db.get_single_value("Inn Hotels Setting", "tax_name_pos_receipt", True)
     if tax_name == "":
         tax_name = "Tax & Service"
 
     remarks = frappe.db.get_single_value("Inn Hotels Setting", "pos_receipt_remarks", True)
+    
+    transfered = False
+    if transferred_to:
+        transfered = True
+
+        transferred_to = {
+            "folio_id": transferred_to
+        }
 
     return {
         "address": address,
@@ -32,5 +36,8 @@ def extended_bil_extra_data(invoice):
             "name": tax_name,
             "total": total_tax
         },
-        "remarks": remarks
+        "remarks": remarks,
+        "transferred": transfered,
+        "transfer_folio": transferred_to,
+        "table": table
     }
