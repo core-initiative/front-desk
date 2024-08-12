c405a5599c2370659e79be452c819b980bd3fbfd
anhilmy <hilmyahmadnaufal@gmail.com>
2024-08-12 21:55:07 +0700

    feat: add last purchase button sync

--- inn/public/js/purchase-order/purchase-order.js
+++ inn/public/js/purchase-order/purchase-order.js
@@ -1,18 +1,38 @@
-frappe.ui.form.on("Purchase Order Item", {
+frappe.ui.form.on("Purchase Order", {
+    refresh: function (frm) {
+        if (doc.status === 0) {
+            frm.add_custom_button(
+                __("Check Last Purchase"),
+                async function () {
+                    for (const row of frm.doc.items) {
+                        await get_last_purchase(frm, row)
+                    }
+                    refresh_field("items")
+                }
+            )
+        }
+    }
+})
 
+frappe.ui.form.on("Purchase Order Item", {
     item_code: async function (frm, cdt, cdn) {
         var row = locals[cdt][cdn]
 
-        let result = await frappe.call({
-            method: "inn.overrides.erpnext.accounts.purchase_order.purchase_order.get_last_purchase_request",
-            args: {
-                item_code: row.item_code
-            }
-        })
-        result = result.message
-        row.custom_last_purchased_quantity = result.last_purchased_quantity
-        row.custom_last_purchase_order = result.last_purchase_order
-        row.custom_last_purchase_date = result.transaction_date
+        await get_last_purchase(frm, row)
         refresh_field("items")
     }
 })
+
+
+async function get_last_purchase(frm, row) {
+    let result = await frappe.call({
+        method: "inn.overrides.erpnext.accounts.purchase_order.purchase_order.get_last_purchase_request",
+        args: {
+            item_code: row.item_code
+        }
+    })
+    result = result.message
+    row.custom_last_purchased_quantity = result.last_purchased_quantity
+    row.custom_last_purchase_order = result.last_purchase_order
+    row.custom_last_purchase_date = result.transaction_date
+}
